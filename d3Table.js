9e1ee425f745805fd1d4ce1c17a4a8ec7459f62c
Lee Blecher <ldb78@cornell.edu>
2020-07-21 00:30:08 -0700

    bold table headers

--- d3Table.js
+++ d3Table.js
@@ -1,15 +1,22 @@
 d3.text("data/chase.csv", function (data) {
     var parsedCSV = d3.csv.parseRows(data);
-
-    var container = d3
+    var table = d3
         .select("body")
-        .append("table")
+        .append("table");
 
-        .selectAll("tr")
-        .data(parsedCSV)
+    // headers
+    table.append("thead").append("tr")
+        .selectAll("th")
+        .data(parsedCSV[0])
+        .enter().append("th")
+        .text(function (d) {
+            return d;
+        });
+    // data
+    table.append("tbody")
+        .selectAll("tr").data(parsedCSV.slice(1))
         .enter()
         .append("tr")
-
         .selectAll("td")
         .data(function (d) {
             return d;
