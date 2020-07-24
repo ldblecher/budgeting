643243d11f588f760fa1d1a8b7c21ebd8612fd9c
Lee Blecher <ldb78@cornell.edu>
2020-07-23 22:42:22 -0700

    create id for currency cells

--- d3Table.js
+++ d3Table.js
@@ -28,6 +28,12 @@
         });
 });*/
 
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+});
+
 d3.csv("data/chase.csv", function (data) {
     data.forEach(function (d) {
         //Convert to numbers and switch positive and negative amounts
@@ -35,7 +41,7 @@ d3.csv("data/chase.csv", function (data) {
         d["Day"] = transactionDateForm.getDate();
         d["Year"] = transactionDateForm.getFullYear();
         d["Month"] = transactionDateForm.getMonth() + 1;
-        d["Amount"] = +-d["Amount"];
+        d["Amount"] = formatter.format(+-d["Amount"]);
     });
     var transactionsByDate = d3.nest()
         .key(function (d) {
@@ -47,11 +53,12 @@ d3.csv("data/chase.csv", function (data) {
         .entries(data);
     var transactions_2020 = transactionsByDate[1];
     transactions_2020.values.sort((a, b) => d3.descending(a.key, b.key));
+
     d3.select("body").append("h1").text(transactions_2020.key);
     //.slice().sort((a, b) => d3.descending(a.Month, b.Month))
     var columns = ['Transaction Date', 'Description', 'Category', 'Type', 'Amount'];
     transactions_2020.values.forEach(function (t) {
-        d3.select("body").append("p").text(t.key);
+        d3.select("body").append("p").attr('class', 'month').text(t.key);
         //https://gist.github.com/jfreels/6734025
         var table = d3.select("body").append("table");
         var thead = table.append("thead");
@@ -67,15 +74,22 @@ d3.csv("data/chase.csv", function (data) {
         var rows = tbody.selectAll('tr')
             .data(t.values)
             .enter()
-            .append('tr');
+            .append('tr')
+            //filter out Payment types
+            .filter(function (d) { return d.Type != "Payment" });
         var cells = rows.selectAll('td')
             .data(function (row) {
                 return columns.map(function (column) {
                     return { column: column, value: row[column] };
-                });
+                })
             })
             .enter()
             .append('td')
+            .attr("id", function (d) {
+                if (d.column == 'Amount') {
+                    return 'amount';
+                }
+            })
             .text(function (d) { return d.value; });
         /*// data
         table.append("tbody")
